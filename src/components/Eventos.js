ba6c6bd120aaeeaa96e229278edf25d7d54714a8
Jonathan <jonathan.moran9348@gmail.com>
2022-08-13 19:10:56 -0300

    Eventos ES7

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,37 +1,30 @@
 import React, { Component } from 'react'
 
 export default class Eventos extends Component {
-    constructor(props){
-        super(props);
 
-        this.state = {
+
+        state = {
             contador: 0,
         }
 
-        this.sumar = this.sumar.bind(this)
-        this.restar = this.restar.bind(this)
-    
 
-    
-
-    }
-    sumar(e) {
-        console.log("sumando")
-        this.setState({
-            contador: this.state.contador + 1,
+        sumar = (e) => {
+            console.log("sumando")
+             this.setState({
+             contador: this.state.contador + 1,
         });
     }
 
-    restar(e) {
-        console.log("restando")
-        this.setState({
+        restar = (e) => {
+            console.log("restando")
+            this.setState({
             contador: this.state.contador - 1
         })
     }
   render() {
     return (
       <div>
-        <h2>Eventos en Componentes de Clase</h2>
+        <h2>Eventos en Componentes de Clase ES7</h2>
         <h3>{this.state.contador}</h3>
         <nav>
             <button onClick={this.sumar}>+</button>
